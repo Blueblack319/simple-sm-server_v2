b3b447e70b452557fe96d054fb51fdc3df957a46
Yang Jae Hoon <didwogns789@naver.com>
2020-10-29 16:01:23 +0900

    Connect with database

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
 import { ApolloServer } from "apollo-server";
 import typeDefs from "./graphql/typeDefs";
 import resolvers from "./graphql/resolvers";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = new ApolloServer({
   typeDefs,
   resolvers,
 });
 
-server.listen({ port: 5000 }).then(({ url }) => {
-  console.log(`✅Server listening at ${url}`);
-});
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log(`✅Database is connected!`);
+    return server.listen({ port: 5000 });
+  })
+  .then(({ url }) => {
+    console.log(`✅Server listening at ${url}`);
+  })
+  .catch((err) => new Error(err));
